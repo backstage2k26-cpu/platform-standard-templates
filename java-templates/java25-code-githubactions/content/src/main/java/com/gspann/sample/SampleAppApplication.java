6d808f5cdbcd6cf05f17eeeff732d497a3de456f
Backstage <backstage2k26@gmail.com>
2026-06-25 22:54:39 +0530

    Fix package declaration in SampleAppApplication

--- java-templates/java25-code-githubactions/content/src/main/java/com/gspann/sample/SampleAppApplication.java
+++ java-templates/java25-code-githubactions/content/src/main/java/com/gspann/sample/SampleAppApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.gspann.sample;
+package com.gspann.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
